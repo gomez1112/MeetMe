013cf4796120850ae7253262364d3181c8171e74
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2023-12-31 15:17:02 -0500

    Initial Commit

---
+++ MeetMe/MeetMeApp.swift
@@ -0,0 +1,32 @@
+//
+//  MeetMeApp.swift
+//  MeetMe
+//
+//  Created by Gerard Gomez on 12/31/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MeetMeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
